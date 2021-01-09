10224edbd6a39e0d20964e577110ce91ab316df1
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-01-09 17:06:02 -0600

    39 Header structure

--- Client/Layouts/BasicLayout/BasicLayout.jsx
+++ Client/Layouts/BasicLayout/BasicLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Header from '../../Components/Header';
 
 export default function BasicLayout(props) {
 	const { children } = props;
 	return (
-		<Container className='basic-layout'>
+		<Container fluid className='basic-layout'>
+			<Header />
 			<Container className='content'>{children} </Container>
 		</Container>
 	);
