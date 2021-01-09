3bdcdc4d87b4b691d1f09320480eb7610a7e9177
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-01-09 16:10:55 -0600

    36 Implementing Semantic UI

--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,7 +1,11 @@
+import { Button, Icon } from 'semantic-ui-react';
+
 export default function Home() {
 	return (
 		<div className='home'>
 			<h1>Hola Mundo</h1>
+			<Button primary>Primary</Button>
+			<Icon name='home' />
 		</div>
 	);
 }
