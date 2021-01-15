079b3f4c00ca4bec812b85fdb78daa57eebb68ab
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-01-15 01:14:13 -0600

    54 Login Form

--- Client/Components/Auth/LoginForm/LoginForm.jsx
+++ Client/Components/Auth/LoginForm/LoginForm.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 export default function LoginForm(props) {
 	const { showRegisterForm } = props;
 	return (
-		<div>
-			<h1>Estamos en login</h1>
-			<button onClick={showRegisterForm}>Registro</button>
-		</div>
+		<Form className='login-form'>
+			<Form.Input name='identifier' type='text' placeholder='Correo electronico' />
+			<Form.Input name='password' type='password' placeholder='Contraseña' />
+			<div className='actions'>
+				<Button type='button' basic onClick={showRegisterForm}>
+					Registrarse
+				</Button>
+				<div className=''>
+					<Button className='submit' type='submit'>
+						Entrar
+					</Button>
+					<Button type='button'>¿Has olvidado la contraseña?</Button>
+				</div>
+			</div>
+		</Form>
 	);
 }
