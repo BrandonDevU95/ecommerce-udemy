c13024419c9fb05227cfd37eb1d013d92418ac1a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-01-23 20:36:39 -0600

    60 Recovering Password

--- Client/Api/User.js
+++ Client/Api/User.js
@@ -33,3 +33,21 @@ export async function loginApi(formData) {
 		return result;
 	} catch (error) {}
 }
+
+export async function resetPasswordApi() {
+	try {
+		const url = `${BASE_PATH}/auth/forgot-password`;
+		const params = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email }),
+		};
+		const response = await fetch(url, params);
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		return null;
+	}
+}
