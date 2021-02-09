e65db87249d0cf04b2fe1022e4830f72bd6d0293
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-08 22:09:47 -0600

    46 Loading BucketS3 configuration

--- Client/Components/ListGames/ListGames.jsx
+++ Client/Components/ListGames/ListGames.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Image, Grid } from 'semantic-ui-react';
+import Link from 'next/link';
 import { map } from 'lodash';
 
 export default function ListGames({ games }) {
 	return (
 		<div className='list-games'>
-			{map(games, (game) => (
-				<h3>{game.title}</h3>
-			))}
+			<Grid>
+				<Grid.Row columns={5}>
+					{map(games, (game) => (
+						<Game key={game.url} game={game} />
+					))}
+				</Grid.Row>
+			</Grid>
 		</div>
+	);
+}
+
+function Game({ game }) {
+	console.log(game);
+	return (
+		<Grid.Column className='list-game__game'>
+			<Link href={`/${game.url}`}>
+				<a>
+					<div className='list-game__game-poster'>
+						<Image src={game.poster.url} alt={game.title} />
+					</div>
+				</a>
+			</Link>
+		</Grid.Column>
 	);
 }
