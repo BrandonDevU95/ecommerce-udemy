111a08a961cba735996b3ba3e527549d372c64d8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-09 17:20:03 -0600

    103 Obtaining the game data

--- Client/pages/[game].jsx
+++ Client/pages/[game].jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import BasicLayout from '../Layouts/BasicLayout';
+import { getGameByUrlApi } from '../Api/Game';
 
 export default function Game() {
+	const [game, setGame] = useState(null);
 	const { query } = useRouter();
 
+	useEffect(() => {
+		(async () => {
+			const response = await getGameByUrlApi(query.game);
+			setGame(response);
+		})();
+	}, []);
+
 	return (
-		<div>
+		<BasicLayout className='game'>
 			<h1>Juegos...</h1>
-		</div>
+		</BasicLayout>
 	);
 }
