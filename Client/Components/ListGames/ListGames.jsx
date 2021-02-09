2a3b38c140d12835f7cc89dcd79a397d76249427
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-08 23:34:17 -0600

    97 Responsive games list

--- Client/Components/ListGames/ListGames.jsx
+++ Client/Components/ListGames/ListGames.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { Image, Grid } from 'semantic-ui-react';
 import Link from 'next/link';
 import { map } from 'lodash';
+import useWindowSize from '../../Hook/useWindowSize';
+import { breakUpSm, breakUpMd, breakUpLg } from '../../Utils/Breakpoints';
 
 export default function ListGames({ games }) {
+	const { width } = useWindowSize();
+
+	const getColumnsRender = () => {
+		switch (true) {
+			case width > breakUpLg:
+				return 5;
+			case width > breakUpMd:
+				return 3;
+			case width > breakUpSm:
+				return 2;
+			default:
+				return 1;
+		}
+	};
+
 	return (
 		<div className='list-games'>
 			<Grid>
-				<Grid.Row columns={5}>
+				<Grid.Row columns={getColumnsRender()}>
 					{map(games, (game) => (
 						<Game key={game.url} game={game} />
 					))}
---
+++ Client/Utils/Breakpoints.js
@@ -0,0 +1,4 @@
+export const breakUpSm = 576;
+export const breakUpMd = 768;
+export const breakUpLg = 992;
+export const breakUpXl = 1200;
