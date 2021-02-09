21f7e8f6e1c21778a04e50ce23a607533a0e875c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-08 23:48:51 -0600

    97 Getting the games by platform

--- Client/pages/games/[platform].jsx
+++ Client/pages/games/[platform].jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getGamesPlatformApi } from '../../Api/Game';
 import BasicLayout from '../../Layouts/BasicLayout';
 import { useRouter } from 'next/router';
 
+const limitPerPage = 10;
+
 export default function Platform() {
+	const [games, setGames] = useState(null);
 	const { query } = useRouter();
+
+	useEffect(() => {
+		(async () => {
+			const response = await getGamesPlatformApi(query.platform, limitPerPage, 0);
+			setGames(response);
+		})();
+	}, [query]);
 
 	return (
 		<BasicLayout className='platform'>
