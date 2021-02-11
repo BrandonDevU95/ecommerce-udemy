77a7f2b78145702b695f1d82730cb4d593e21898
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 16:28:53 -0600

    120 Getting the search results

--- Client/pages/search.jsx
+++ Client/pages/search.jsx
@@ -1,10 +1,28 @@
 import { useState, useEffect } from 'react';
 import BasicLayout from '../Layouts/BasicLayout';
+import { searchGamesApi } from '../Api/Game';
+import { useRouter } from 'next/router';
+import { size } from 'lodash';
 
 export default function search() {
+	const [games, setGames] = useState(null);
+	const { query } = useRouter();
+
 	useEffect(() => {
 		document.getElementById('search-game').focus();
 	}, []);
+
+	useEffect(() => {
+		(async () => {
+			if (size(query.query) > 0) {
+				const response = await searchGamesApi(query.query);
+				if (size(response) > 0) setGames(response);
+				else setGames([]);
+			} else {
+				setGames([]);
+			}
+		})();
+	}, [query]);
 
 	return (
 		<BasicLayout className='search'>
