1b7b7ae2371a5e4cef9de85913f8c24ad124b611
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-10 22:38:46 -0600

    110 Release date and description

--- Client/Components/Game/InfoGame/InfoGame.jsx
+++ Client/Components/Game/InfoGame/InfoGame.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
 import CarouselScreenshots from '../CarouselScreenshots';
+import moment from 'moment';
+import 'moment/locale/es';
 
 export default function InfoGame({ game }) {
 	return (
 		<div className='info-game'>
 			<ReactPlayer className='info-game__video' url={game.video} controls={true} />
 			<CarouselScreenshots title={game.title} screenshots={game.screenshot} />
+			<div className='info-game__content'>
+				<div dangerouslySetInnerHTML={{ __html: game.summary }} />
+				<div className='info-game__content-date'>
+					<h4>Fecha de lanzamiento:</h4>
+					<p>{moment(game.releaseDate).format('LL')}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
