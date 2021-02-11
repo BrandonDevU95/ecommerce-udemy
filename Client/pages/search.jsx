c20db0cfd9b80adb43216f6960d86e32a056ebf7
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 16:18:47 -0600

    119 Loading the search page when starting a search

--- Client/pages/search.jsx
+++ Client/pages/search.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import BasicLayout from '../Layouts/BasicLayout';
 
 export default function search() {
+	useEffect(() => {
+		document.getElementById('search-game').focus();
+	}, []);
+
 	return (
 		<BasicLayout className='search'>
 			<h1>Search...</h1>
