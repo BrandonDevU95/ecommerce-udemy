2fff9321ecfc8d0c8747f380e4978ba4a5ef4fa2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 16:00:20 -0600

    118 Creating the search page

---
+++ Client/pages/search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from '../Layouts/BasicLayout';
+
+export default function search() {
+	return (
+		<BasicLayout className='search'>
+			<h1>Search...</h1>
+		</BasicLayout>
+	);
+}
