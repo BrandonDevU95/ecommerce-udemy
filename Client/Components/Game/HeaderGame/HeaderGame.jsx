ca8b38d6abc67d2a9d8248e93239687cf3c23f6f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-10 23:12:19 -0600

    112 Checking if we have a favorite game

--- Client/Components/Game/HeaderGame/HeaderGame.jsx
+++ Client/Components/Game/HeaderGame/HeaderGame.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, Fragment } from 'react';
 import { Grid, Image, Icon, Button } from 'semantic-ui-react';
+import { isFavoriteApi } from '../../../Api/Favorite';
 import { size } from 'lodash';
+import classNames from 'classnames';
+import useAuth from '../../../Hook/useAuth';
 
 export default function HeaderGame({ game }) {
 	return (
@@ -16,11 +19,38 @@ export default function HeaderGame({ game }) {
 }
 
 function Info({ game }) {
+	const [isFavorite, setIsFavorite] = useState(false);
+	const { auth, logout } = useAuth();
+
+	useEffect(() => {
+		(async () => {
+			const response = await isFavoriteApi(auth.idUser, game.id, logout);
+			console.log(response);
+			if (size(response) > 0) setIsFavorite(response);
+			else setIsFavorite(false);
+		})();
+	}, [game]);
+
+	const addFavorite = () => {
+		console.log('Añadir');
+	};
+
+	const removeFavorite = () => {
+		console.log('Eliminando');
+	};
+
 	return (
 		<Fragment>
 			<div className='header-game__title'>
 				{game.title}
-				<Icon name='heart outline' link />
+				<Icon
+					name={isFavorite ? 'heart' : 'heart outline'}
+					className={classNames({
+						like: isFavorite,
+					})}
+					link
+					onClick={isFavorite ? removeFavorite : addFavorite}
+				/>
 			</div>
 			<div className='header-game__delivery'>Entrega en 24/48 Hrs</div>
 			<div className='header-game__summary' dangerouslySetInnerHTML={{ __html: game.summary }} />
