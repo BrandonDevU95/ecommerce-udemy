b9dc806850ebc6c295f7cc81b7bd7edc70397951
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 23:37:44 -0600

    140 Back function to place the order and payment

--- Server/api/order/controllers/order.js
+++ Server/api/order/controllers/order.js
@@ -1,8 +1,43 @@
-'use strict';
-
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51IJsynJVumg5aiWAfgLo2yqo18bW62m0LMp3taBUL2RxOxBG13avrCB290WvKh1co95kLHvl5AC1JEuAMg8fwgSR00mUrIgcW2"
+);
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+    products.forEach(products, (product) => {
+      totalPayment = totalPayment + product.price;
+    });
+
+    const charge = await stripe.charge.create({
+      amount: totalPayment * 100,
+      currency: "USD",
+      source: token.id,
+      description: `ID Usuario ${idUser}`,
+    });
+
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        game: product.id,
+        user: idUser,
+        totalPayment,
+        idPayment: charge.id,
+        addressShipping,
+      };
+      const validData = await createStrapi.entityValidator.validateEntity(
+        strapi.models.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+    return createOrder;
+  },
+};
