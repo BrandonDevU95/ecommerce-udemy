3ef186bc15381d6ec0865961ae5f340435fafe50
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 20:19:36 -0600

    127 Getting the data of the products of the cart

--- Client/pages/cart.jsx
+++ Client/pages/cart.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getGameByUrlApi } from '../Api/Game';
+import useCart from '../Hook/useCart';
 import BasicLayout from '../Layouts/BasicLayout';
 
 export default function cart() {
+	const { getProductsCart } = useCart();
+	const products = getProductsCart();
+
+	return !products ? <EmptyCart /> : <FullCart products={products} />;
+}
+
+function EmptyCart() {
+	return (
+		<BasicLayout className='empty-cart'>
+			<h2>No hay productos</h2>
+		</BasicLayout>
+	);
+}
+
+function FullCart({ products }) {
+	const [productsData, setProductsData] = useState(null);
+
+	console.log(productsData);
+
+	useEffect(() => {
+		(async () => {
+			const productsTemp = [];
+			for await (const product of products) {
+				const data = await getGameByUrlApi(product);
+				productsTemp.push(data);
+			}
+			setProductsData(productsTemp);
+		})();
+	}, []);
 	return (
 		<BasicLayout className='cart'>
 			<h1>Carrito...</h1>
