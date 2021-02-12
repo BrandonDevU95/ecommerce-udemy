14827b6eaa7c2669a6e89d986b5cdd194e0bf64e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 20:05:48 -0600

    126 Creating the cart page

---
+++ Client/pages/cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from '../Layouts/BasicLayout';
+
+export default function cart() {
+	return (
+		<BasicLayout className='cart'>
+			<h1>Carrito...</h1>
+		</BasicLayout>
+	);
+}
