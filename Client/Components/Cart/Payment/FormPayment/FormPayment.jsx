be406a51e4c687281322ee554353128df1bd18a0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-11 23:12:34 -0600

    139 Creating the billing agreement with stripe

--- Client/Components/Cart/Payment/FormPayment/FormPayment.jsx
+++ Client/Components/Cart/Payment/FormPayment/FormPayment.jsx
@@ -8,15 +8,33 @@ import useAuth from '../../../../Hook/useAuth';
 import useCart from '../../../../Hook/useCart';
 
 export default function FormPayment({ products, address }) {
-	const handleSubmit = (e) => {
+	const [loading, setLoading] = useState(false);
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log('Pago');
+		setLoading(true);
+		if (!stripe || !elements) return;
+
+		const cartElement = elements.getElement(CardElement);
+		const result = await stripe.createToken(cartElement);
+
+		if (result.error) {
+			toast.error(result.error.message);
+		} else {
+			console.log(result);
+		}
+
+		setLoading(false);
 	};
 
 	return (
 		<form className='form-payment' onSubmit={handleSubmit}>
 			<CardElement />
-			<Button type='submit'>Pagar</Button>
+			<Button type='submit' loading={loading} disabled={!stripe}>
+				Pagar
+			</Button>
 		</form>
 	);
 }
