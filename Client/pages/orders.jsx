c05ca28f33111ef3968263de64202d359561453b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-02-12 00:37:38 -0600

    144 Getting all the user's orders

--- Client/pages/orders.jsx
+++ Client/pages/orders.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import BasicLayout from '../Layouts/BasicLayout';
+import { Grid } from 'semantic-ui-react';
+import { getOrdersApi } from '../Api/Order';
+import useAuth from '../Hook/useAuth';
 
 export default function orders() {
+	const [orders, setOrders] = useState(null);
+	const { auth, logout } = useAuth();
+
+	useEffect(() => {
+		(async () => {
+			const response = await getOrdersApi(auth.idUser, logout);
+			setOrders(response || []);
+		})();
+	}, []);
+
 	return (
 		<BasicLayout className='orders'>
 			<div className='orders__block'>
